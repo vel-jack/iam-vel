1c4b50bab7def3c55d0d812fc66755f8f25697a5
Velmurugan R <cenavel2@gmail.com>
2022-07-06 12:49:24 +0530

    app dev content

--- src/components/appdev.jsx
+++ src/components/appdev.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import flutterlogo from '../assets/flutter.svg';
+import playlogo from '../assets/google-play.png';
+import githublogo from '../assets/github.png';
 
 const AppDev = () => {
+  
+  const PlayLink = ({link})=>{
+    return (
+    <a href={link} className='inline-flex'>
+      <img src={playlogo} alt="playlogo" width={15} />
+    </a>);
+  };
+  const GitLink = ({link})=>{
+    return (
+    <a href={link} className='inline-flex'>
+      <img src={githublogo} alt="playlogo" width={15} />
+    </a>);
+  };
+
   return (
-    <div>AppDev</div>
+    <div className='flex flex-col md:flex-row justify-between p-5 xl:px-48 lg:px-24'>
+      <div className=' self-center w-full'>
+        <img src={flutterlogo} alt="F" className=' w-48 hidden md:block lg:w-40'/>
+        </div>
+      <div className='w-full'>
+        <p className=' text-2xl font-bold'>Flutter & Dart</p>
+        <p className=' text-lg'>Learning and developing apps using flutter.</p>
+        <br />
+        <div className='md:text-lg'>
+            <p>{'- Bahrain Bus'} <PlayLink link='#'/></p>
+            <p>{'- Mini Todo'} <PlayLink link='#'/>,<GitLink link='#'/> </p>
+            <p>{'- Hastagram'} <GitLink link='#'/> </p>
+            <p>{'- Gulf news'} <GitLink link='#'/></p>
+            <p>{'- Notes App'} <GitLink link='#'/></p>
+            <p>{'- Word Rush'} <PlayLink link='#'/>,<GitLink link='#'/> </p>
+            <p>{'- Bahrain Bus'} <PlayLink link='#'/>,<GitLink link='#'/></p> 
+            <p>{'- Bluetooth Scale'} <GitLink link='#'/></p>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -2,23 +2,24 @@ import React from 'react'
 
 const Intro = () => {
   return (
-    <div className='md:min-h-screen flex justify-between p-5 xl:px-48'>
-      <div className='md:flex flex-col justify-between'>
+    <div className='md:min-h-screen flex justify-between p-5 xl:px-48 lg:px-24'>
+      <div className='md:flex flex-col justify-start sm:justify-center lg:justify-between'>
         <div></div>
         <div>
           <p className='md:text-6xl font-bold text-3xl'>Hello 👋</p>
           <p className='md:text-4xl text-2xl'>I am <span className='font-bold'>Vel</span>,</p>
         </div>
-        <div className='h-5 md:h-0'></div>
+        <div className='h-5'></div>
         <div className='md:text-lg'>
-          <p>Indie-Game developer,</p>
+          <p>Indie-Game developer,</p> 
           <p>@Nothing Box Games 🔲.</p>
           <p>Published <span className='font-bold'>apps</span> and <span className='font-bold'>games</span> at <span className='animate-pulse'>playstore</span> 🎮.</p>
           <p>Hands on experience in <span className='font-bold'>Flutter</span> and <span className='font-bold'>Godot</span>.</p>
           <p>Simple & Minimalist.</p>
-        </div>        
+        </div>
+        <div></div>
       </div>
-      <div className=' text-5xl font-bold w-1/2 self-center text-center md:text-9xl sm:block hidden'>Hi</div>
+      <div className=' text-5xl md:text-9xl font-bold w-1/2 self-center text-center  sm:block hidden'>Hi</div>
     </div>
   )
 }
