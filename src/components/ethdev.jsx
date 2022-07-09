07f716798dee59286d636ceb9e299c037e9a9cd2
Velmurugan R <cenavel2@gmail.com>
2022-07-09 09:43:23 +0530

    nft link

--- src/components/ethdev.jsx
+++ src/components/ethdev.jsx
@@ -22,9 +22,9 @@ const EthDev = () => {
             <p>{'- Crypto T-Shirts'} <Demo link={'https://crypto-tee.vercel.app/'}/> <IconLink link={'https://github.com/vel-jack/crypto-tee'}/></p>
             <p>{'- Bank contract'} <Demo link={'http://simple-bank-contract.vercel.app/'}/> <IconLink link={'https://github.com/vel-jack/Simple-Bank-Contract'}/></p>
             <p>{'- Bone coin (ERC20)'} <Demo link={'http://bone-coin.vercel.app/'}/> <IconLink link={'https://github.com/vel-jack/bone-coin'}/></p>
-            
-        </div>
-        
+            <p><a href="https://opensea.io/assets/matic/0x112721c13dd721a543a0c805791dceebdbaf308d/56" target={'_blank'}>{'- Cadena: Ethereum 101 Certificate 🔗'}</a></p>
+            <p><a href="https://opensea.io/assets/matic/0x3cd266509d127d0eac42f4474f57d0526804b44e/19071" target={'_blank'}>{'- Buildspace: Build a Web3 App 🔗'}</a></p>
+        </div>        
       </div>
     </div>
   )
